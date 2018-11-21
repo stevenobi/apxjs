c65a232b3f15c9573430d41bf5f3168f4550529c
stevenobi <s.obermeyer@t-online.de>
2018-11-21 17:22:27 +0100

    updated
    added comments

--- FadeAlert.js
+++ FadeAlert.js
@@ -3,7 +3,7 @@ function fadeAlert(s, d = 2000, f = 'slow') {
     $('.t-Alert').delay(d).fadeOut(f);
   }
 
-//IE safe
+//IE safe (put into global function declaration of standard page template)
 function fadeAlert(d, f, s) {
     var Delay    = d || 2000,
         Fade     = f || 'slow',
@@ -12,7 +12,7 @@ function fadeAlert(d, f, s) {
 }
 
 
-
+//execute on every page (probably need a dynamic action to run for click on IG region to run javascript code below)
 fadeAlert();
 // for each IG on page
 $.each($('div.a-IG').closest('div.t-Region'), function(index, value) {
